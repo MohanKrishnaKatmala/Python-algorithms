b930d513eac3034c466b6ae588fd4e4784c93b44
MohanKrishnaKatamala <katamalamohankrishna@gmail.com>
2025-11-14 12:39:57 +0530

    Fibonacci series with the lines to get but here any line has the start to end.
    Refactor Fibonacci series function to accept user-defined limit for lines printed. Adjusted variable initialization and output formatting.

--- Fibonacci_series.py
+++ Fibonacci_series.py
@@ -3,14 +3,16 @@
 # Fibonacci series was the continuous one that should be the definitely get the output for every loop instance so here thtat the right angle shape was happen to that fibonacci series.
 # Start numbers are 0 and 1. 
 def fibonacci_series(first_number=0,second_number=1):# no parameters nut can give for the where you start from the add.
-  first_number=0
-  second_number=1
   # starting numbers.
-  print(first_number,end="  ") # This to add the next number to end as the space as two words space.
+  limit=int(input("enter a limit value that was lines not the numbers."))+2 # here we print the first numbers and second numbers.
+  print(first_number) # This to add the next number to end as the space as two words space.
   print(second_number)
   total_numbers=2 # This is to get total numbers in that right angle triangle shpae numbers.
-  limit=int(input("enter a limit value to print up to that lines  "))+1 # Here 0 and 1 should be display for the better output.
+  save_first=first_number
+  save_second=second_number
   for a in range(limit):
+    first_number=save_first
+    second_number=save_second
     for b in range(a+1):
       summating=first_number+second_number
       print(summating,end="  ")
