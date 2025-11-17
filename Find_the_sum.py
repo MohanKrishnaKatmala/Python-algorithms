55df06e19ab199a460349aeb073de1a5572b915c
MohanKrishnaKatamala <katamalamohankrishna@gmail.com>
2025-11-17 12:19:20 +0530

    Add function to find consecutive sum in a list
    Implement function to find consecutive numbers that sum to a given value.

---
+++ Find_the_sum.py
@@ -0,0 +1,18 @@
+"""
+Finding the sum is in their in list or not in consecutive order.
+The list contains the digits only.
+The consecutive order can check the sum.
+Output:
+If there is a sum give that numbers as output.
+"""
+def find_the_sum(outside_list,sum_value):
+    for index in range(len(outside_list)):
+        result=0
+        for sub_index in range(index,len(outside_list)):
+            result+=outside_list[sub_index]
+            if(result==sum_value):
+                print(outside_list[index:sub_index+1])
+        
+outside_list=[1,2,3,4,5]
+sum_value=9
+find_the_sum(outside_list,sum_value)
